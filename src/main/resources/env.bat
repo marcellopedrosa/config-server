e5fb55a0cbe79f7ffbb1daf1e0838d274e112eb7
Marcello Pedrosa <49072390+marcellopedrosa@users.noreply.github.com>
2025-07-02 09:44:21 -0300

    Update env.bat

--- src/main/resources/env.bat
+++ src/main/resources/env.bat
@@ -3,6 +3,8 @@ echo Definindo variáveis de ambiente...
 
 setx CONFIG_SERVER_GIT_URI "https://github.com/marcellopedrosa/config-server.git" /M
 setx CONFIG_SERVER_GIT_USERNAME "admin" /M
+
+echo Defina a senha em: https://github.com/settings/tokens -> Crie: Personal access tokens (classic) -> Marque: [x] Repo - Full control of private repositories
 setx CONFIG_SERVER_GIT_PASSWORD "admin" /M
 
 echo Variaveis definidas com sucesso!
